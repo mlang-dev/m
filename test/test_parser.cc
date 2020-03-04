6ac0a83caa80ab037c9f84a7f3f48900252d5317
Ligang Wang <ligangwangs@gmail.com>
2020-03-03 22:51:47 -0500

    add more unit tests

--- test/test_parser.cc
+++ test/test_parser.cc
@@ -72,3 +72,51 @@ TEST(testParser, testBlockBinaryFunctionName){
   ASSERT_EQ(BINARY_NODE, body->type);
   destroy_parser(parser);
 }
+
+TEST(testParser, testFacIfCondition){
+  char test_code[128] = R"(fac n = 
+    if n< 2 n
+    else n * fac (n-1))";
+  auto parser = _create_parser_for_string(test_code);
+  block_node * block = parse_block(parser, nullptr);
+  auto node = (function_node*)block->nodes[0];
+  auto body = node->body->nodes[0];
+  ASSERT_EQ(1, block->nodes.size());
+  ASSERT_STREQ("fac", node->prototype->name.c_str());
+  ASSERT_EQ(CONDITION_NODE, body->type);
+  destroy_parser(parser);
+}
+
+TEST(testParser, testForLoop){
+  char test_code[128] = R"(loopprint n = 
+  for i in 0..n
+    print i
+  )";
+  auto parser = _create_parser_for_string(test_code);
+  block_node * block = parse_block(parser, nullptr);
+  auto node = (function_node*)block->nodes[0];
+  auto body = node->body->nodes[0];
+  ASSERT_EQ(1, block->nodes.size());
+  ASSERT_STREQ("loopprint", node->prototype->name.c_str());
+  ASSERT_EQ(FOR_NODE, body->type);
+  destroy_parser(parser);
+}
+
+TEST(testParser, testVariableInFunction){
+  char test_code[128] = R"(distance x1 y1 x2 y2 = 
+  xx = (x1-x2) * (x1-x2)
+  yy = (y1-y2) * (y1-y2)
+  sqrt (xx + yy)
+  )";
+  auto parser = _create_parser_for_string(test_code);
+  block_node * block = parse_block(parser, nullptr);
+  auto node = (function_node*)block->nodes[0];
+  auto body = node->body->nodes[0];
+  ASSERT_EQ(1, block->nodes.size());
+  ASSERT_STREQ("distance", node->prototype->name.c_str());
+  ASSERT_EQ(VAR_NODE, body->type);
+  destroy_parser(parser);
+}
+
+
+
