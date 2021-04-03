cd147b6953d13dff5d69e1e5b6f5959762e563b8
knoics <ligangwangs@gmail.com>
2021-04-03 18:26:50 -0400

    add ir code verification in codegen unit test

--- test/codegen/test_codegen.cc
+++ test/codegen/test_codegen.cc
@@ -11,15 +11,29 @@
 #include "compiler/compiler.h"
 
 class testCodeGenerator : public TestBase {};
+const char *module_name = "test_ir";
 
-TEST_F(testCodeGenerator, testStructTypeDefIR)
+void _make_module_ir(LLVMModuleRef module, const char *ir_string, char *module_ir)
 {
+    sprintf(module_ir, R"(; ModuleID = '%s'
+source_filename = "%s"
+target datalayout = "%s"
+target triple = "%s"
+
+%s
+)", module_name, module_name, LLVMGetDataLayoutStr(module), LLVMGetDefaultTargetTriple(), ir_string);
+}
+
+TEST_F(testCodeGenerator, testGlobalVar)
+{
+    char module_ir[1024];
     char test_code[] = "m = 10";
     env* env = env_new(false);
-    create_ir_module(env->cg, "test_ir");
+    create_ir_module(env->cg, module_name);
+    _make_module_ir(env->cg->module, "@m = externally_initialized global i32 0", module_ir);
     block_node* block = parse_string(env->parser, "test", test_code);
     char *ir_string = emit_ir_string(env, &block->base);
-    printf("%s", ir_string);
+    ASSERT_STREQ(module_ir, ir_string);
     free_ir_string(ir_string);
     env_free(env);
 }
