a1fe2e697283e3d4492387eb26909676c000886b
knoics <ligangwangs@gmail.com>
2021-04-04 22:07:40 -0400

    add fun/call codegen unit tests

--- test/codegen/test_cg_call.cc
+++ test/codegen/test_cg_call.cc
@@ -53,3 +53,27 @@ entry:
 )";
     validate_m_code_with_ir_code(test_code, expected_ir);
 }
+
+TEST_F(testCGCall, testCallByStruct)
+{
+    const char test_code[] = R"(
+type Point2D = x:double y:double
+f x:Point2D = x
+)";
+    const char *expected_ir = R"(
+define i8 @__f_char(i8 %x) {
+entry:
+  %x1 = alloca i8, align 1
+  store i8 %x, i8* %x1, align 1
+  %x2 = load i8, i8* %x1, align 1
+  ret i8 %x2
+}
+
+define i8 @main() {
+entry:
+  %calltmp = call i8 @__f_char(i8 99)
+  ret i8 %calltmp
+}
+)";
+    validate_m_code_with_ir_code(test_code, expected_ir);
+}
