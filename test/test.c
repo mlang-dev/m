f8d9dfacc053242386d68fab7c02a5762333b6ea
knoics <ligangwangs@gmail.com>
2021-11-28 18:04:18 -0500

    run unit tests in wasm

--- test/test.c
+++ test/test.c
@@ -1,6 +1,14 @@
+#include "wasm/wasm.h"
+
+#ifdef WASM
+#define ENTRY _start
+#else
+#define ENTRY main
+#endif
+
 void test_array();
 
-int main()
+EXPORT int ENTRY()
 {
   test_array();
   return 0;
