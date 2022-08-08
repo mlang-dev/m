268d961cb8d851aa3592d1b9e00c88732f5f8609
knoics <ligangwangs@gmail.com>
2022-08-08 01:25:13 -0400

    add two more unit tests

--- test/parser/test_lalr_parser_expr.c
+++ test/parser/test_lalr_parser_expr.c
@@ -11,6 +11,32 @@
 #include "test.h"
 #include <stdio.h>
 
+TEST(test_lr_parser_expr, arithmetic_simple_add)
+{
+    const char test_code[] = "1+2";
+    frontend_init();
+    struct lalr_parser *parser = parser_new();
+    struct ast_node *ast = parse_code(parser, test_code);
+    string dump_str = dump(ast);
+    ASSERT_STREQ("(1+2)", string_get(&dump_str));
+    ast_node_free(ast);
+    lalr_parser_free(parser);
+    frontend_deinit();
+}
+
+TEST(test_lr_parser_expr, arithmetic_simple_mult)
+{
+    const char test_code[] = "1*2";
+    frontend_init();
+    struct lalr_parser *parser = parser_new();
+    struct ast_node *ast = parse_code(parser, test_code);
+    string dump_str = dump(ast);
+    ASSERT_STREQ("(1*2)", string_get(&dump_str));
+    ast_node_free(ast);
+    lalr_parser_free(parser);
+    frontend_deinit();
+}
+
 TEST(test_lr_parser_expr, arithmetic_exp_neg)
 {
     const char test_code[] = "-1+2";
@@ -67,6 +93,8 @@ TEST(test_lr_parser_expr, arithmetic_exp_parentheses)
 int test_lr_parser_expr()
 {
     UNITY_BEGIN();
+    RUN_TEST(test_lr_parser_expr_arithmetic_simple_add);
+    RUN_TEST(test_lr_parser_expr_arithmetic_simple_mult);
     RUN_TEST(test_lr_parser_expr_arithmetic_exp_neg);
     RUN_TEST(test_lr_parser_expr_arithmetic_exp);
     RUN_TEST(test_lr_parser_expr_arithmetic_exp2);
