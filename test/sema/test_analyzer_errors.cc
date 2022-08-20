2adcc0dccbd7aacdc897c8e2491fbd7755ec224b
knoics <ligangwangs@gmail.com>
2022-08-20 00:58:30 -0400

    change analyzer error to use new parser

--- test/sema/test_analyzer_errors.cc
+++ test/sema/test_analyzer_errors.cc
@@ -19,12 +19,13 @@ no_exist_function_call ()
 )";
     testing::internal::CaptureStderr();
     env *env = env_new(false);
-    ast_node *block = parse_string(env->parser, "test", test_code);
+    struct ast_node *block = parse_code(env->new_parser, test_code);
     auto node = *(ast_node **)array_front(&block->block->nodes);
     ASSERT_EQ(CALL_NODE, node->node_type);
     emit_code(env, (ast_node *)block);
     auto error = testing::internal::GetCapturedStderr();
     ASSERT_STREQ("error: :1:1: no_exist_function_call not defined\n", error.c_str());
+    ast_node_free(block);
     env_free(env);
 }
 
@@ -35,12 +36,13 @@ TEST(testAnalyzerError, testRemError)
 )";
     testing::internal::CaptureStderr();
     env *env = env_new(false);
-    ast_node *block = parse_string(env->parser, "test", test_code);
+    struct ast_node *block = parse_code(env->new_parser, test_code);
     auto node = *(ast_node **)array_front(&block->block->nodes);
     ASSERT_EQ(BINARY_NODE, node->node_type);
     emit_code(env, (ast_node *)block);
     auto error = testing::internal::GetCapturedStderr();
     ASSERT_STREQ("error: :2:1: type not same for binary op: +\n", error.c_str());
+    ast_node_free(block);
     env_free(env);
 }
 
@@ -51,11 +53,12 @@ x:int = true
 )";
     testing::internal::CaptureStderr();
     env *env = env_new(false);
-    ast_node *block = parse_string(env->parser, "test", test_code);
+    struct ast_node *block = parse_code(env->new_parser, test_code);
     auto node = *(ast_node **)array_front(&block->block->nodes);
     ASSERT_EQ(VAR_NODE, node->node_type);
     emit_code(env, (ast_node *)block);
     auto error = testing::internal::GetCapturedStderr();
     ASSERT_STREQ("error: :2:1: variable type not matched with literal constant\n", error.c_str());
+    ast_node_free(block);
     env_free(env);
 }
