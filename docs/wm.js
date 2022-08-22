af78565e4e13f3ec7493d42a8b037098f7a8570e
knoics <ligangwangs@gmail.com>
2022-08-21 22:16:22 -0400

    update doc

---
+++ docs/wm.js
@@ -0,0 +1,23 @@
+function to_js_str(instance, c_str)
+{
+	const str_len = instance.exports.str_len(c_str)	
+	let ta = new Uint8Array(instance.exports.memory.buffer, c_str, str_len)
+	return (new TextDecoder()).decode(ta)
+}
+
+function str_to_ab(instance, str, c_str)
+{
+	let ta = (new TextEncoder()).encode(str) //Uint8Array
+	let str_len = ta.length + 1;
+	let dst = new Uint8Array(instance.exports.memory.buffer, c_str, str_len)
+	dst.set(ta);
+	dst[ta.length] = 0;
+}
+
+function run_wasm_code(instance, wasm, wasm_size)
+{
+	let ta = new Uint8Array(instance.exports.memory.buffer, wasm, wasm_size);
+	var compiled = new WebAssembly.Module(ta);
+	var instance = new WebAssembly.Instance(compiled, {});
+	return instance.exports.run();
+}
