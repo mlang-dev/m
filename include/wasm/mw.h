52e577f18cfc30916541edd3d6117ea65c25f30e
knoics <ligangwangs@gmail.com>
2022-09-06 23:38:20 -0400

    add missing files

---
+++ include/wasm/mw.h
@@ -0,0 +1,5 @@
+#include "wasm/libc.h"
+
+wasm_export_name(version) const char *version();
+wasm_export_name(compile_code) u8 *compile_code(const char *text);
+wasm_export_name(get_code_size) u32 get_code_size();
