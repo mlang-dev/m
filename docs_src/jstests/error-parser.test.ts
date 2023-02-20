6e7ca8d65d6cbb48d61727e7874365f0aaf51ece
knoics <ligangwangs@gmail.com>
2023-02-20 12:34:19 -0500

    refactoring grammar

--- docs_src/jstests/error-parser.test.ts
+++ docs_src/jstests/error-parser.test.ts
@@ -3,4 +3,4 @@ import {mtest_strings} from './mtest';
 mtest_strings('parse fun def missing =', 'parse fun def missing =', `
 let f x
 `,[ "symbol NEWLINE is not expected after grammar rule param_decl = IDENT\n",
-    "symbol : is expected to parse ident_type = IDENT .: type_expr but got NEWLINE\n"]);
+    "symbol : is expected to parse ident_type = IDENT .: type_item but got NEWLINE\n"]);
