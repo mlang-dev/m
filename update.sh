57bfe4c3f4d80e34b8a02e40e64f24acbea54e81
knoics <ligangwangs@gmail.com>
2023-09-24 23:39:21 +0000

    update m.wasm

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
 #! /usr/bin/bash
-cp ../c2m/mlib/* ./src/sys
+curl https://raw.githubusercontent.com/mlang-dev/c2m/main/mlib/math.m > ./src/sys/math.m
+curl https://raw.githubusercontent.com/mlang-dev/c2m/main/mlib/stdio.m > ./src/sys/stdio.m
+curl https://raw.githubusercontent.com/mlang-dev/c2m/main/mlib/stdlib.m > ./src/sys/stdlib.m
+
+
